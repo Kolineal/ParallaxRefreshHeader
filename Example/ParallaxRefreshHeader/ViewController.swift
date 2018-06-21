cd416118c7cabf41a5a1630198c2a9cb1c1a6e4e
Igor Cherepanov <kolineal@gmail.com>
2018-06-21 10:45:45 +0200

    Initial commit

---
+++ Example/ParallaxRefreshHeader/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ParallaxRefreshHeader
+//
+//  Created by Kolineal on 06/21/2018.
+//  Copyright (c) 2018 Kolineal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
